ff5a7e2794c6f33c0cd0f4ab86bdaf00df6d0d27
William Sim <w.m.n.sim@gmail.com>
2020-07-02 11:57:23 -0600

    Add shared package

---
+++ Services/Packages/__init__.py
@@ -0,0 +1,2 @@
+def add_arr(num1, num2):
+    return num1 + num2
---
+++ Services/Packages/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='add_stuff',
+    version='0.0.1',
+    description='Private package from private github repo test',
+    url='git@github.com:will-to-ride/Python_MonoRepo_Kubeless.git',
+    author='William Sim',
+    author_email='w.m.n.sim@gmail.com',
+    license='unlicense',
+    packages=['add_stuff'],
+    zip_safe=False
+)
